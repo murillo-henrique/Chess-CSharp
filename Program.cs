34edc99d69748ec1420b9063a14a9e079315d73a
murillo-henrique <146981566+murillo-henrique@users.noreply.github.com>
2024-02-23 15:15:45 -0300

    Melhorando a implementação do metodo ImprimirTabuleiro

--- Program.cs
+++ Program.cs
@@ -7,13 +7,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Clear();
+
         try
         {
+
             Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
             tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
             tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-            tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 0));
+            //tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 0));
+            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Branca), new Posicao(2, 3));
 
             Tela.ImprimirTabuleiro(tabuleiro);
         }
--- Tela.cs
+++ Tela.cs
@@ -8,6 +8,7 @@ class Tela
     {
         for (int linha = 0; linha < tabuleiro.Linhas; linha++)
         {
+            Console.Write(8 - linha + " ");
             for (int coluna = 0; coluna < tabuleiro.Colunas; coluna++)
             {
                 if (tabuleiro.Peca(linha, coluna) == null)
@@ -16,11 +17,28 @@ class Tela
                 }
                 else
                 {
-                    Console.Write(tabuleiro.Peca(linha, coluna) + " ");
+                    ImprimirPeca(tabuleiro.Peca(linha, coluna));
+                    Console.Write(" ");
                 }
             }
 
             System.Console.WriteLine();
+        }
+        System.Console.WriteLine("  a b c d e f g h");
+    }
+
+    public static void ImprimirPeca(Peca peca)
+    {
+        if (peca.Cor == Cor.Branca)
+        {
+            Console.Write(peca);
+        }
+        else
+        {
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(peca);
+            Console.ForegroundColor = aux;
         }
     }
 }
